5131e2a29a40a666de86f4a76408503a35642bfb
Vinukrishna05 <vinukrishna0508@gmail.com>
2025-10-07 15:30:48 +0530

    vk 7/10 3.30

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { CiSearch } from "react-icons/ci";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import './Header.css';
@@ -8,17 +8,17 @@ export default function Header() {
   return (
     <header className="site-header">
       <div className="header__left">
-        <Link to="/" className="logo" aria-label="Warmy home">
+        <NavLink to="/" className="logo" aria-label="Warmy home">
           <img src="/Warmy Logo.png" alt="Warmy logo" />
-        </Link>
+        </NavLink>
       </div>
 
       <nav className="header__center" aria-label="Main navigation">
         <ul className="nav-list">
-          <li><Link to="/" className="nav-link active">Home</Link></li>
-          <li><Link to="/about" className="nav-link">About</Link></li>
-          <li><Link to="/services" className="nav-link">Services</Link></li>
-          <li><Link to="/contact" className="nav-link">Contact</Link></li>
+          <li><NavLink to="/" className="nav-link">Home</NavLink></li>
+          <li><NavLink to="/about" className="nav-link">About</NavLink></li>
+          <li><NavLink to="/services" className="nav-link">Services</NavLink></li>
+          <li><NavLink to="/contact" className="nav-link">Contact</NavLink></li>
         </ul>
       </nav>
 
