d6eb3b70215f3d6b119a868fc613286290ab4f1b
Harikrish <harikrishnanfeb2003@gmail.com>
2025-10-07 09:53:28 +0530

    header in progress

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,78 +1,27 @@
-import React from "react";
 import styled from "styled-components";
 import { FaWhatsapp } from "react-icons/fa";
 import { FiPhoneCall } from "react-icons/fi";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { Link } from "react-router-dom";
+import { CiSearch } from "react-icons/ci";
+import './Header.css'
 
-const HeaderContainer = styled.header`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: #136169;
-  padding: 12px 30px;
-  height: 90px;
-  color: white;
-
-  @media (max-width: 650px) {
-    background-color: #1361694D;
-    color: white;
-  }
-`;
-
-const Logo = styled(Link)`
-  font-size: 1.8rem;
-  font-weight: bold;
-
-  span {
-    color: white;
-  }
-`;
-
-const Actions = styled.div`
-  display: flex;
-  gap: 20px;
-  width: auto;
-`;
-
-const HeaderLink = styled(Link)`
-  color: white;
-  font-size: 1.4rem;
-  cursor: pointer;
-
-  svg {
-    font-size: 34px; /* default size */
-  }
-
-  @media (max-width: 650px) {
-    color: black;
-
-    svg {
-      font-size: 24px; /* smaller size on small screens */
-    }
-  }
-`;
-
-const Header = () => {
+function Header() {
   return (
-    <HeaderContainer>
-      <Logo to={"/"}>
-        <img src="/Warmy Logo.png" alt="Warmy Logo" height="60px" />
-      </Logo>
+    <>
+    <div className="header-container">
+      <div className="logo">
+        <img src="/Warmy Logo.png" height={'40px'} alt="" />
+      </div>
+      <div className="navs">B</div>
+      <div className="login">
+        <CiSearch size={34}/>  
+        <MdOutlineShoppingCart size={34}/>
+        <button className="login-button">Login</button>
+      </div>
+    </div>
+    </>
+  )
+}
 
-      <Actions>
-        <HeaderLink as="a">
-          <FiPhoneCall />
-        </HeaderLink>
-        <HeaderLink to={"/cart"}>
-          <MdOutlineShoppingCart />
-        </HeaderLink>
-        <HeaderLink as="a">
-          <FaWhatsapp />
-        </HeaderLink>
-      </Actions>
-    </HeaderContainer>
-  );
-};
-
-export default Header;
+export default Header
